4a515205d71548554ee551203a5b8eb57d049833
Stefano Huber <stefano.huber@gmail.com>
2025-12-04 09:37:32 +0100

    Add problem I for Swerc 2022-2023, greedy interactove problem. Still gets WA on Codeforces, but looks correct from the solution provided by swerc.

---
+++ swerc2023/i/main.py
@@ -0,0 +1,36 @@
+def bernardo(n, m, x, k):
+    print("Bernardo", flush=True)
+    for i in range(n):
+        y, a = [int(l) for l in input().rstrip().split()]
+        if y - 1e-6 > m/k:
+            print((m//k + 1) * (a // (m//k + 1) ) + (m//k + 1), flush= True)
+        else:
+            print(a, flush = True)
+
+
+def alessia(n, m, x):
+    print("Alessia", flush=True)
+    bernardos = [0, m+1]
+    for i in range(n):
+        for j in range(len(bernardos)-1):
+            if x[i] + bernardos[j] + 1 <= bernardos[j+1]:
+                print(x[i], bernardos[j]+1, flush=True)
+                break
+        b = int(input().rstrip())
+        # print(b, "debug")
+        bernardos.append(b)
+        bernardos = sorted(bernardos)
+
+
+
+n, m = [int(x) for x in input().rstrip().split()]
+x = [int(x) for x in input().rstrip().split()]
+
+x = sorted(x, reverse=True)
+played = False
+for k in range(1, n+1):
+    if m / k + 1e-6 < x[k-1] and not played and n > 1:
+        bernardo(n, m, x, k)
+        played = True
+if not played:
+    alessia(n, m, x)
