78932fa70f1bfb8c2d6b0ae0852d81177d1be73d
Tullio <tullio.villa@usi.ch>
2026-02-12 14:12:54 +0100

    Fix problem j from cerc 2020: ACCEPTED remove debug output + fix rounding errors + clean up code

--- cerc2020/j/main.py
+++ cerc2020/j/main.py
@@ -1,47 +1,54 @@
 import math
 
-W, H, start_x, start_y, end_x, end_y = map(int, input().split())
+W, H, sx, sy, ex, ey = map(int, input().split())
+
 grid = []
-for _ in range(H//2):
+for _ in range(H // 2):
     grid.append(list(map(int, input().split())))
+# (x, y) coordinates of central points correspond to grid[y // 2][x // 2],
+# so we transpose the grid to simplify indexing
+grid = [[grid[j][i] for j in range(H // 2)] for i in range(W // 2)]
 
-grid = [[grid[j][i] for j in range(H//2)] for i in range(W//2)]
-
-# equation of the path segment: (y - s_y)(e_x - s_x) = (x - s_x)(e_y - s_y)
+# equation of the path segment: (y - sy)(ex - sx) = (x - sx)(ey - sy)
 intersections = set()
-vert_step = 2 if start_x < end_x else -2
-for vertical in range(start_x + vert_step //2, end_x + vert_step//2, vert_step):
-    intersections.add((vertical, start_y + (end_y - start_y)/(end_x - start_x)* (vertical - start_x)))
+eps = 1e-6
+xdir = 1 if sx <= ex else -1
+for x in range(sx + xdir, ex + xdir, 2 * xdir):
+    y = sy + (ey - sy) / (ex - sx) * (x - sx)
+    if abs(y - round(y)) < eps:   # round to integer
+        y = round(y)
+    intersections.add((x, y))
+ydir = 1 if sy <= ey else -1
+for y in range(sy + ydir, ey + ydir, 2 * ydir):
+    x = sx + (ex - sx) / (ey - sy) * (y - sy)
+    if abs(x - round(x)) < eps:   # round to integer
+        x = round(x)
+    intersections.add((x, y))
 
-#hor_intersections = set()
-hor_step = 2 if start_y < end_y else -2
-for hor in range(start_y + hor_step//2, end_y + hor_step//2, hor_step):
-    intersections.add((start_x + (end_x - start_x)/(end_y - start_y)* (hor - start_y), hor))
+total_length = math.sqrt((ex - sx) ** 2 + (ey - sy) ** 2)
 
-total_length = math.sqrt((end_x - start_x)**2 + (end_y - start_y)**2)
+for x, y in intersections:
+    if (x / 2).is_integer() and (y / 2).is_integer():
+        roof_prev = grid[(x - xdir) // 2][(y - ydir) // 2]
+        roof_next = grid[(x + xdir) // 2][(y + ydir) // 2]
+        roof_box3 = grid[(x - xdir) // 2][(y + ydir) // 2]
+        roof_box4 = grid[(x + xdir) // 2][(y - ydir) // 2]
 
-for point in intersections:
-    if (point[0]/2).is_integer() and (point[1]/2).is_integer():
-        #  We have a multiple intersection
-        print(f"We are at point {point}")
-        height_prev = grid[round((point[0] - vert_step//2)//2)][round((point[1] - hor_step//2)//2)]
-        height_next = grid[round((point[0] + vert_step//2)//2)][round((point[1] + hor_step//2)//2)]
-        height_neighbor1 = grid[round((point[0] - vert_step // 2) // 2)][round((point[1] + hor_step // 2) // 2)]
-        height_neighbor2 = grid[round((point[0] + vert_step // 2) // 2)][round((point[1] - hor_step // 2) // 2)]
+        second_max = sorted([roof_prev, roof_next, roof_box3, roof_box4])[2]
+        total_length += abs(second_max - roof_prev) + abs(second_max - roof_next)
 
-        second_max = sorted([height_prev, height_next, height_neighbor1, height_neighbor2])[2]
-        total_length += abs(second_max - height_prev) + abs(second_max - height_next)
+    elif (x / 2).is_integer():
+        y = round(y - (y % 2) + 1)
+        roof_next = grid[(x + xdir) // 2][y // 2]
+        roof_prev = grid[(x - xdir) // 2][y // 2]
 
-    elif (point[0]/2).is_integer():
-        height_prev = grid[round((point[0] - vert_step // 2) // 2)][round((point[1] - (point[1] % 2) + 1) //2)]
-        height_next = grid[round((point[0] + vert_step // 2) // 2)][round((point[1] - (point[1] % 2) + 1) //2)]
-
-        total_length += abs(height_prev - height_next)
+        total_length += abs(roof_next - roof_prev)
 
     else:
-        height_prev = grid[round((point[0] - (point[0] % 2) + 1) // 2)][round((point[1] - hor_step // 2) // 2)]
-        height_next = grid[round((point[0] - (point[0] % 2) + 1) // 2)][round((point[1] + hor_step // 2) // 2)]
+        x = round(x - (x % 2) + 1)
+        roof_prev = grid[x // 2][(y - ydir) // 2]
+        roof_next = grid[x // 2][(y + ydir) // 2]
 
-        total_length += abs(height_prev - height_next)
+        total_length += abs(roof_next - roof_prev)
 
 print(total_length)
